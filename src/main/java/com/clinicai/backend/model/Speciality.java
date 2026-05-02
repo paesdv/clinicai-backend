dc582fade436d42dad3467e3a50eab57d5c6eb45
paes <paesm191@gmail.com>
2026-05-02 16:21:12 -0300

    model speciality

---
+++ src/main/java/com/clinicai/backend/model/Speciality.java
@@ -0,0 +1,39 @@
+package com.clinicai.backend.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "specialties")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Speciality {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private int durationMinutes;
+
+    private boolean active = true;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
